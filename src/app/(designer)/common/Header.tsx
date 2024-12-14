c5408d6a13cbc1e9dd8fd32f7df2e67821571f73
Syxneet <nguyenvansy09092001@gmail.com>
2024-12-14 11:44:44 +0700

    update

--- src/app/(designer)/common/Header.tsx
+++ src/app/(designer)/common/Header.tsx
@@ -1,59 +1,105 @@
 "use client";
 import MenuMobile from "@/app/(designer)/common/MenuMobile";
 import useStyleByTheme from "@/hooks/useStyleByTheme";
+import { Theme } from "@/lib/constant";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
+const links = [
+    { name: "HOME", path: "/home" },
+    { name: "PROJECTS", path: "/designer-projects" },
+    { name: "ABOUT", path: "/about-me" },
+    { name: "WORK WITH ME", path: "/home" },
+];
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const background = useStyleByTheme(["bg-[#ffffff]", "bg-transparent", "bg-transparent"]);
-  const backgroundButton = useStyleByTheme(["bg-[#ffffff]", "bg-theme-button-background", "bg-theme-button-background"]);
-  const colorButton = useStyleByTheme(["text-theme-primary", "text-[#131313]", "text-[#fff]"]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [theme, setTheme] = useState("");
 
-  const [backgroundColor, setBackgroundColor] = useState(background);
-  const [textColor, setTextColor] = useState("text-theme-primary");
+    useEffect(() => {
+        // Lấy giá trị theme từ URL
+        const urlParams = new URLSearchParams(window.location.search);
+        const themeParam = urlParams.get("theme");
+        setTheme(themeParam || "designerStyle1"); // Nếu không có, gán giá trị mặc định
+    }, []);
+    const background = useStyleByTheme([
+        "bg-[#ffffff]",
+        "bg-transparent",
+        "bg-transparent",
+    ]);
+    const backgroundButton = useStyleByTheme([
+        "bg-[#ffffff]",
+        "bg-theme-button-background",
+        "bg-theme-button-background",
+    ]);
+    const colorButton = useStyleByTheme([
+        "text-theme-primary",
+        "text-[#131313]",
+        "text-[#fff]",
+    ]);
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 100) {
-        setBackgroundColor("bg-[#fff]");
-        setTextColor("text-[#131313]");
-      } else {
-        setBackgroundColor(background);
-        setTextColor("text-theme-primary");
-      }
-    });
-  }, [background]);
+    const [backgroundColor, setBackgroundColor] = useState(background);
+    const [textColor, setTextColor] = useState("text-theme-primary");
 
-  return (
-    <div className={`${isOpen ? "w-screen h-screen fixed bg-[#01000560] z-50" : ""}`}>
-      <div className={`px-[7.03rem] max-md:p-[1.46rem] flex justify-between items-center py-[1.76rem] fixed w-full z-10 top-0 ${backgroundColor} ${textColor}`}>
-        <p className=' text-[1.46413rem] md:text-[2.22222rem] leading-[1.1] max-md:tracking-[-0.07321rem] md:leading-[1.5]'>Jenny Nguyen</p>
-        <div className='flex items-center justify-center gap-[2.22rem] max-md:hidden'>
-          <Link href={"/"} className=' text-[1.11111rem] font-medium leading-[1.5]'>
-            HOME
-          </Link>
-          <Link href={"/"} className=' text-[1.11111rem] font-medium leading-[1.5]'>
-            PROJECTS
-          </Link>
-          <Link href={"/"} className=' text-[1.11111rem] font-medium leading-[1.5]'>
-            ABOUT
-          </Link>
-          <Link
-            href={"/"}
-            className={`${backgroundButton} ${colorButton} text-[1.11111rem] font-medium leading-[1.5] px-[1.88rem] py-[1.04rem] rounded-[2.64rem] border-[1px] border-solid border-[#131313]`}>
-            WORK WITH ME
-          </Link>
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                setBackgroundColor("bg-[#fff]");
+                setTextColor("text-[#131313]");
+            } else {
+                setBackgroundColor(background);
+                setTextColor("text-theme-primary");
+            }
+        });
+    }, [background]);
+
+    return (
+        <div
+            className={`${
+                isOpen ? "w-screen h-screen fixed bg-[#01000560] z-50" : ""
+            }`}
+        >
+            <div
+                className={`px-[7.03rem] max-md:p-[1.46rem] flex justify-between items-center py-[1.76rem] fixed w-full z-10 top-0 ${backgroundColor} ${textColor}`}
+            >
+                <p className=" text-[1.46413rem] md:text-[2.22222rem] leading-[1.1] max-md:tracking-[-0.07321rem] md:leading-[1.5]">
+                    Jenny Nguyen
+                </p>
+                <div className="flex items-center justify-center gap-[2.22rem] max-md:hidden">
+                    {links.map((item) => (
+                        <Link
+                            key={item.path}
+                            href={`${item.path}${
+                                theme ? `?theme=${theme}` : ""
+                            }`}
+                            className="text-[1.11111rem] font-medium leading-[1.5]"
+                        >
+                            {item.name}
+                        </Link>
+                    ))}
+                </div>
+
+                <svg
+                    onClick={() => setIsOpen(true)}
+                    className="md:hidden"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                >
+                    <path
+                        d="M5 17H19M5 12H19M5 7H19"
+                        stroke="#131313"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    />
+                </svg>
+            </div>
+
+            <MenuMobile isOpen={isOpen} setIsOpen={setIsOpen} />
         </div>
-
-        <svg onClick={() => setIsOpen(true)} className='md:hidden' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none'>
-          <path d='M5 17H19M5 12H19M5 7H19' stroke='#131313' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round' />
-        </svg>
-      </div>
-
-      <MenuMobile isOpen={isOpen} setIsOpen={setIsOpen} />
-    </div>
-  );
+    );
 };
 
 export default Header;
--- src/components/pages/designer-home/Skills/index.tsx
+++ src/components/pages/designer-home/Skills/index.tsx
@@ -3,64 +3,75 @@ import SkillItem from "./SkillItem";
 import Count from "./Count";
 
 export type SkillType = {
-  icon: string;
-  title: string;
-  description: string;
+    icon: string;
+    title: string;
+    description: string;
 };
 
 export type CountType = {
-  count: number;
-  description: string;
+    count: number;
+    description: string;
 };
 
 const Skills = () => {
-  const skills: SkillType[] = [
-    {
-      icon: "/assets/images/designer/octa.svg",
-      title: "Branding design",
-      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-    },
-    {
-      icon: "/assets/images/designer/octa.svg",
-      title: "UX/UI design",
-      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-    },
-    {
-      icon: "/assets/images/designer/octa.svg",
-      title: "Motion design",
-      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-    },
-    {
-      icon: "/assets/images/designer/octa.svg",
-      title: "Social Media",
-      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-    },
-  ];
+    const skills: SkillType[] = [
+        {
+            icon: "/assets/images/designer/octa.svg",
+            title: "Branding design",
+            description:
+                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+        },
+        {
+            icon: "/assets/images/designer/octa.svg",
+            title: "UX/UI design",
+            description:
+                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+        },
+        {
+            icon: "/assets/images/designer/octa.svg",
+            title: "Motion design",
+            description:
+                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+        },
+        {
+            icon: "/assets/images/designer/octa.svg",
+            title: "Social Media",
+            description:
+                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+        },
+    ];
 
-  const counts = [
-    { count: 15, description: "Happy clients" },
-    { count: 50, description: "Projects this year" },
-  ];
+    const counts = [
+        { count: 15, description: "Happy clients" },
+        { count: 50, description: "Projects this year" },
+    ];
 
-  return (
-    <div className='flex max-md:px-[1.39rem] py-[3.33rem] p-[6.67rem] md:gap-[6.67rem] max-md:flex-col'>
-      <div className='md:w-[35.3472rem] flex flex-col flex-shrink-0 max-md:mb-[4.44rem]'>
-        <h4 className='text-theme-primary-content text-[2.22222rem] md:text-[3.33333rem] leading-[1.2] font-medium max-md:mb-[1.67rem] mb-[2.22rem]'>
-          The skills I possess will help make your project a success.
-        </h4>
-        <div className='flex flex-col gap-[0.55556rem]'>
-          {counts.map((count, index) => (
-            <Count key={"count" + index} item={count} />
-          ))}
+    return (
+        <div className="flex max-md:px-[1.39rem] py-[3.33rem] p-[6.67rem] md:gap-[6.67rem] max-md:flex-col">
+            <div className="md:w-[35.3472rem]  max-md:mb-[4.44rem] flex flex-shrink-0 ">
+                <div className="w-full flex flex-col sticky top-[7rem] h-fit">
+                    <h4 className="text-theme-primary-content text-[2.22222rem] md:text-[3.33333rem] leading-[1.2] font-medium max-md:mb-[1.67rem] mb-[2.22rem]">
+                        The skills I possess will help make your project a
+                        success.
+                    </h4>
+                    <div className="flex flex-col gap-[0.55556rem]">
+                        {counts.map((count, index) => (
+                            <Count key={"count" + index} item={count} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col gap-[1.67rem] max-md:grid">
+                {skills.map((skill, index) => (
+                    <SkillItem
+                        key={"skill" + index}
+                        index={index + 1}
+                        skill={skill}
+                    />
+                ))}
+            </div>
         </div>
-      </div>
-      <div className='flex flex-col gap-[1.67rem] max-md:grid'>
-        {skills.map((skill, index) => (
-          <SkillItem key={"skill" + index} index={index + 1} skill={skill} />
-        ))}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Skills;
